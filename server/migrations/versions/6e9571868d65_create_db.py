0169dfcfc83d74802666d081d67b0939bc4a25ec
Martin <martingarcia804@yahoo.com>
2024-01-29 22:53:32 -0600

    Clear README.md

---
+++ server/migrations/versions/6e9571868d65_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 6e9571868d65
+Revises: 
+Create Date: 2024-01-29 22:50:46.134120
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6e9571868d65'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
