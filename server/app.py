c812ea9626bc7ea28b88500b5d2f3a627eb00fcc
Martin <martingarcia804@yahoo.com>
2024-02-01 17:48:51 -0600

    Add first successful migration as previous ones failed due to inproper imports

--- server/app.py
+++ server/app.py
@@ -3,21 +3,30 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Local imports
-from config import app, db, api
-# Add your model imports
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-# Views go here!
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+api = Api(app)
 
+from models import User, Game, Review
+
+# Routes
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
 
 
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
--- server/models.py
+++ server/models.py
@@ -1,15 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy()
 
-from config import db
-
-# Models go here!
-
-class User(db.model):
+# Models
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -28,13 +23,12 @@ class User(db.model):
     def _repr__(self):
         return f'User: {self.username}, ID: {self.id}'
 
-
 class Game(db.Model, SerializerMixin):
     __tablename__ = 'games'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.string(110), nullable=False)
-    genre = db.Column(db.string(50))
+    title = db.Column(db.String(110), nullable=False)
+    genre = db.Column(db.String(50))
     developer = db.Column(db.String(50))
     release_date = db.Column(db.Date)
 
@@ -44,5 +38,15 @@ class Game(db.Model, SerializerMixin):
     def __repr__(self):
         return f'Game Title: {self.title}'
 
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
 
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
+
+    def __repr__(self):
+        return f'Review ID {self.id} by {self.user_id}| Game: {self.game_id} '
 
